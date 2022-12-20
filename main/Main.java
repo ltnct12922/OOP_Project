5f99a42cc90fe269a3c6fc0bec1e4c84e31fcdb7
qthinh30 <91869213+ititiu20312quocthinh@users.noreply.github.com>
2022-12-20 19:19:35 +0700

    Add files via upload

---
+++ main/Main.java
@@ -0,0 +1,13 @@
+package com.thanggun99.tank90.main;
+
+import com.thanggun99.tank90.guis.GUI;
+
+import java.io.IOException;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        GUI gui = new GUI();
+        gui.setVisible(true);
+    }
+}
